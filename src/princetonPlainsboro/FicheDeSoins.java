03c03d24541cbbfa223a1bb1791f812e05d266d6
luce <lucille.boulas@hotmail.fr>
2014-06-03 14:57:38 +0200

    Premier commit sur version mardi 9h34

---
+++ src/princetonPlainsboro/FicheDeSoins.java
@@ -0,0 +1,100 @@
+package princetonPlainsboro;
+
+import java.util.Vector;
+import javax.swing.DefaultListModel;
+
+class FicheDeSoins {
+    private Patient patient;
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public void setMedecin(Medecin medecin) {
+        this.medecin = medecin;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    private Medecin medecin;
+
+    public void setActes(Vector<Acte> actes) {
+        this.actes = actes;
+    }
+    private Date date;
+    private Vector<Acte> actes;       // contient des objets de classe 'Acte'
+   
+
+   
+    public FicheDeSoins(Patient patient, Medecin medecin, Date date) {
+        this.patient = patient;
+        this.medecin = medecin;
+        this.date = date;
+        actes = new Vector<Acte>();   // liste vide
+      }
+
+    
+
+   
+    public Patient getPatient() { return patient; }
+    public Medecin getMedecin() { return medecin; }
+    public Date    getDate()    { return date; }
+    
+    public void ajouterActe(Acte acte) {
+        actes.add(acte);
+        }
+    
+    public void ajouterActe(Code code, int coefficient) {
+        Acte acte = new Acte(code, coefficient);
+        actes.add(acte);
+        }
+
+    public Vector<Acte> getActes() {
+        return actes;
+    }
+    
+    
+    public void afficher() {
+        System.out.println("Fiche de soins du " + date.toString());
+        System.out.println("- medecin : " + medecin.toString());
+        System.out.println("- patient : " + patient.fichePatient());
+        System.out.println("- actes medicaux :");
+        if(!actes.isEmpty()){
+        for (int i=0; i<actes.size(); i++) {
+            Acte a = actes.get(i);
+            System.out.println("    > " + a.toString());
+            }
+        System.out.println("- Cout total: "+this.coutTotal()+ " €");
+        }}
+    
+    public double coutTotal() {
+        double total = 0;
+        for (int i=0; i<actes.size(); i++) {
+            Acte a = actes.get(i);
+            total += a.cout();
+            }
+        return total;
+        }
+    
+     public String afficherString() {
+           System.out.println("afficherString f");
+       String s= "Fiche de soins du " + date.toString();
+       s+="\n"+"- medecin : " + medecin.toString();
+        s+="\n"+"- patient : " + patient.fichePatient();
+       if (!actes.isEmpty()){
+           System.out.println("boucle");
+        s+="\n"+"- actes medicaux :";
+        for (int i=0; i<actes.size(); i++) {
+            Acte a = actes.get(i);
+             s+="\n"+"    > " + a.toString();
+            }
+         s+="\n"+"- Cout total: "+this.coutTotal()+ " €";
+       }else{
+           System.out.println("je suis vide");
+           s+="\n"+"- Cout total: 0 €";
+       }
+        return s;
+        }
+    }
+
---
+++ src/princetonPlainsboro/Specialite.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package princetonPlainsboro;
+
+/**
+ *
+ * @author luce
+ */
+enum Specialite {
+        anesthegiologie,
+        cardiologie,
+        dermatologie,
+        gerontologie,
+        gynecologie,
+        hematologie,
+        neurologie,
+        pediatrie,
+        radiologie,
+        urologie,
+        ORL,
+        oncologie;
+ }
+    
+
